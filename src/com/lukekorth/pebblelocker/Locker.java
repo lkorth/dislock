daf9ec8d68e5f55d07ccb8165d8e35baf51c147c
lkorth <luke@lukekorth.com>
2013-08-31 01:17:22 -0700

    Add additional check to only lock if watch is not currently connected

--- src/com/lukekorth/pebblelocker/Locker.java
+++ src/com/lukekorth/pebblelocker/Locker.java
@@ -6,21 +6,24 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import com.getpebble.android.kit.PebbleKit;
 import com.lukekorth.pebblelocker.PebbleLocker.CustomDeviceAdminReceiver;
 
 public class Locker {
 	
 	public static void lockIfEnabled(Context context) {
-		DevicePolicyManager dpm = ((DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE));
-		
-		if (dpm.isAdminActive(new ComponentName(context, CustomDeviceAdminReceiver.class))) {
-			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		if(!PebbleKit.isWatchConnected(context)) {		
+			DevicePolicyManager dpm = ((DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE));
 			
-			if(prefs.getBoolean("key_enable_locker", false)) {			
-				dpm.resetPassword(prefs.getString("key_password", ""), DevicePolicyManager.RESET_PASSWORD_REQUIRE_ENTRY);
+			if (dpm.isAdminActive(new ComponentName(context, CustomDeviceAdminReceiver.class))) {
+				SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 				
-				if(prefs.getBoolean("key_force_lock", false))
-					dpm.lockNow();
+				if(prefs.getBoolean("key_enable_locker", false)) {			
+					dpm.resetPassword(prefs.getString("key_password", ""), DevicePolicyManager.RESET_PASSWORD_REQUIRE_ENTRY);
+					
+					if(prefs.getBoolean("key_force_lock", false))
+						dpm.lockNow();
+				}
 			}
 		}
 	}
