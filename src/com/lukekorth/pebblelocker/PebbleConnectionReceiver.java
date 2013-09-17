54408881bb55ae9f6f631d64d4e9dd957d4462db
lkorth <luke@lukekorth.com>
2013-09-16 20:00:10 -0700

    Fix small issue with having to re-enter password too frequently

--- src/com/lukekorth/pebblelocker/PebbleConnectionReceiver.java
+++ src/com/lukekorth/pebblelocker/PebbleConnectionReceiver.java
@@ -3,6 +3,7 @@ package com.lukekorth.pebblelocker;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.PowerManager;
 import android.preference.PreferenceManager;
 
 public class PebbleConnectionReceiver extends BroadcastReceiver {
@@ -11,9 +12,14 @@ public class PebbleConnectionReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		String action = intent.getAction();
 				
-		if(action.equalsIgnoreCase("com.getpebble.action.PEBBLE_CONNECTED"))
-			PreferenceManager.getDefaultSharedPreferences(context).edit().putBoolean("unlock", true).commit();
-		else if (action.equalsIgnoreCase("com.getpebble.action.PEBBLE_DISCONNECTED"))
+		if(action.equalsIgnoreCase("com.getpebble.action.PEBBLE_CONNECTED")) {
+			if(((PowerManager) context.getSystemService(Context.POWER_SERVICE)).isScreenOn())
+				PreferenceManager.getDefaultSharedPreferences(context).edit().putBoolean("unlock", true).commit();
+			else {
+				PreferenceManager.getDefaultSharedPreferences(context).edit().putBoolean("unlock", false).commit();
+				Locker.unlockIfEnabled(context);
+			}
+		} else if (action.equalsIgnoreCase("com.getpebble.action.PEBBLE_DISCONNECTED"))
 			Locker.lockIfEnabled(context);
 		else if (action.equalsIgnoreCase("android.intent.action.USER_PRESENT") && 
 				PreferenceManager.getDefaultSharedPreferences(context).getBoolean("unlock", false) &&
