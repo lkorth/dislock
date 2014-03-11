dc921c357ae87de47ea07cbdd5a24463088f2a4d
lkorth <luke@lukekorth.com>
2014-03-10 22:16:57 -0700

    Fix missing UI update

--- pebble-locker-android/pebblelocker/src/main/java/com/lukekorth/pebblelocker/Locker.java
+++ pebble-locker-android/pebblelocker/src/main/java/com/lukekorth/pebblelocker/Locker.java
@@ -48,6 +48,8 @@ public class Locker {
 			unlock();
 		else if (!connectedToDeviceOrWifi && isLocked(false))
 			lock(forceLock);
+        else
+            sendBroadcast();
 	}
 
 	public void lock() {
